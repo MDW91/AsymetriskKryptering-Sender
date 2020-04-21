b0821a52f4ce0f73efe7cbfbafb7c9dc3c3a175b
MDW91 <42375451+MDW91@users.noreply.github.com>
2020-04-21 10:58:03 +0200

    Asymetrisk kryptering - sender projekt
    .

---
+++ AsymetriskKrypteringRSAafsender/AsymetriskKrypteringRSAafsender/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AsymetriskKrypteringRSAafsender
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CipherBytes_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_Exponent(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_Modulus(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Encrypt_Click(object sender, RoutedEventArgs e)
+        {
+            var rsa = new RsaWithXMLKey();
+
+            // krypter plaintekstten og uskriv resultat som en hexidecimal tekststreng
+            Textbox_Cipherbytes.Text = rsa.EncryptfromXml(Textbox_Modulus.Text, Textbox_Exponent.Text, Textbox_Plaintext.Text);
+
+        }
+
+    }
+}
---
+++ AsymetriskKrypteringRSAafsender/AsymetriskKrypteringRSAafsender/RsaWithXMLKey.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsymetriskKrypteringRSAafsender
+{
+    class RsaWithXMLKey
+    {
+        private RSAParameters _publickey;
+
+        public string EncryptfromXml(string modulustext, string exponenttext, string plaintext)
+        {
+            var rsa = new RsaWithXMLKey();
+
+            // kør krypterings metoden
+            var encrypted = rsa.EncryptData(modulustext, exponenttext, Encoding.UTF8.GetBytes(plaintext));
+
+            // retunere den krypterede tekst til view, som hex-streng
+            return BitConverter.ToString(encrypted);
+
+        }
+
+
+        public byte[] EncryptData(string modulustext, string exponenttext, byte[] dataToEncrypt)
+        {
+            byte[] cipherbytes;
+
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                rsa.PersistKeyInCsp = false;
+
+                // Konverter modulus/exponent fra hex streng til byte array 
+                _publickey.Modulus = modulustext.Split('-').Select(item => Convert.ToByte(item, 16)).ToArray();
+                _publickey.Exponent = exponenttext.Split('-').Select(item => Convert.ToByte(item, 16)).ToArray();
+                
+                // impoter rsa parametre 
+                rsa.ImportParameters(_publickey);
+
+                // krypter plaintekst strengen med de nye public key informationer
+                cipherbytes = rsa.Encrypt(dataToEncrypt, false);
+            }
+
+            // retunere den krypterede tekst som byte array
+            return cipherbytes;
+        }
+
+    }
+}
